315794dba4260ee604cc16871f43b399c4dfb080
alunny <alunny@gmail.com>
2012-01-18 23:44:17 -0800

    parseXml functions and tests

--- pluginstall.js
+++ pluginstall.js
@@ -1,5 +1,7 @@
 // copyright (c) 2012 Andrew Lunny, Adobe Systems
 var fs = require('fs'),
+    path = require('path'),
+    et = require('elementtree'),
     platforms = require('./platforms')
 
 // check arguments and resolve file paths
@@ -17,9 +19,25 @@ exports.init = function (platform, projectPath, pluginPath) {
     }
 }
 
-// parse plugin.xml
-exports.parseXml = function (configObj) { }
+exports.parseXml = function (configObj) {
+    var xmlPath     = path.join(configObj.pluginPath, 'plugin.xml'),
+        xmlText     = fs.readFileSync(xmlPath, 'utf-8'),
+        xmlDoc      = new et.ElementTree(et.XML(xmlText)),
+        rootAttr    = xmlDoc._root.attrib,
+        supportedPlatforms;
 
-// move files
+    supportedPlatforms = xmlDoc.findall('platform').map(function (platform) {
+        return platform.attrib['name'];
+    });
 
-// edit files
+    return {
+        xmlDoc: xmlDoc,
+        _id: rootAttr['id'],
+        version: rootAttr['version'],
+        platforms: supportedPlatforms
+    };
+}
+
+exports.moveFiles = function (configObj, dataObj) {}
+
+exports.editFiles = function (configObj, dataObj) {}
