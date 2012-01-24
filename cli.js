674959a83aa323fedf0222788f44265a5d3f42ed
alunny <alunny@gmail.com>
2012-01-23 16:51:13 -0800

    [cli] super basic cli

---
+++ cli.js
@@ -0,0 +1,24 @@
+var pluginstall = require('./pluginstall'),
+    platform, projectDir, pluginDir,
+    config, plugin
+
+if (process.argv[0] == 'node') process.argv.shift()
+
+process.argv.shift() // skip "cli.js"
+
+    platform = process.argv.shift()
+    projectDir = process.argv.shift()
+    pluginDir = process.argv.shift()
+
+    config = pluginstall.init(platform, projectDir, pluginDir)
+    plugin = pluginstall.parseXml(config)
+
+    pluginstall.installPlugin(config, plugin, function (err) {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log('plugin installed')
+        }
+    });
+
+
