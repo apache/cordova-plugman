c3320c494fea470ef12e7408ed5e116b3fa235e4
alunny <alunny@gmail.com>
2012-02-17 15:39:57 -0800

    [npm] replacing fstream with ncp
    fstream had weird issues on our linux server(s)

--- util/asyncCopy.js
+++ util/asyncCopy.js
@@ -1,17 +1,7 @@
-var fs = require('fs'),
-    fstream = require('fstream');
+var ncp = require('ncp').ncp;
 
 // includes support for recursively copying directories
 // srcPath must exist
 module.exports = function asyncCopy(srcPath, targetPath, callback) {
-    var read = fstream.Reader(srcPath);
-
-    if (fs.statSync(srcPath).isDirectory()) {
-        read.pipe(fstream.Writer({ path: targetPath,
-                                    type: 'Directory'}));
-    } else {
-        read.pipe(fstream.Writer(targetPath));
-    }
-
-    read.on('end', callback);
+    ncp(srcPath, targetPath, callback);
 }
