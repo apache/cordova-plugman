111a3c40f885174c8ce85186431b9606797412a0
alunny <alunny@gmail.com>
2012-03-16 15:38:32 -0700

    [cli] usage message + version check

--- cli.js
+++ cli.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 var pluginstall = require('./pluginstall'),
     platform, projectDir, pluginDir,
-    config, plugin
+    config, plugin, package;
 
 if (process.argv[0] == 'node') process.argv.shift()
 
 process.argv.shift() // skip "cli.js"
 
+if (process.argv.length == 0) {
+    console.log('Usage: pluginstall [platform] [project directory] [plugin directory]')
+} else if (process.argv[0] === '-v') {
+    package = require('./package')
+    console.log('pluginstall version ' + package.version)
+} else {
     platform = process.argv.shift()
     projectDir = process.argv.shift()
     pluginDir = process.argv.shift()
@@ -21,5 +27,4 @@ process.argv.shift() // skip "cli.js"
             console.log('plugin installed')
         }
     });
-
-
+}
