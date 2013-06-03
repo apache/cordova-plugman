f7a7a45e8793b7e2781d3da670a81e29154789da
Braden Shepherdson <braden.shepherdson@gmail.com>
2013-06-03 15:47:15 -0400

    Add .fetch.json metadata file to plugin dirs after fetch
    This file holds data about where this plugin was fetched from. This
    facilitates updating plugins and installing relative dependencies.

--- src/fetch.js
+++ src/fetch.js
@@ -2,6 +2,7 @@ var shell   = require('shelljs'),
     fs      = require('fs'),
     plugins = require('./util/plugins'),
     xml_helpers = require('./util/xml-helpers'),
+    metadata = require('./util/metadata'),
     path    = require('path');
 
 // possible options: link, subdir, git_ref
@@ -19,7 +20,20 @@ module.exports = function fetchPlugin(plugin_dir, plugins_dir, options, callback
             if (callback) callback(err);
             else throw err;
         } else {
-            plugins.clonePluginGitRepo(plugin_dir, plugins_dir, options.subdir, options.git_ref, callback);
+            var data = {
+                source: {
+                    type: 'git',
+                    url:  plugin_dir,
+                    subdir: options.subdir,
+                    ref: options.git_ref
+                }
+            };
+
+            plugins.clonePluginGitRepo(plugin_dir, plugins_dir, options.subdir, options.git_ref, function(err, dir) {
+                if (!err) {
+                    metadata.save_fetch_metadata(dir, data);
+                }
+            });
         }
     } else {
         if (plugin_dir.lastIndexOf('file://', 0) === 0) {
@@ -41,6 +55,14 @@ module.exports = function fetchPlugin(plugin_dir, plugins_dir, options, callback
             shell.mkdir('-p', dest);
             shell.cp('-R', path.join(plugin_dir, '*') , dest);
         }
+
+        var data = {
+            source: {
+                type: 'local',
+                path: plugin_dir
+            }
+        };
+        metadata.save_fetch_metadata(dest, data);
 
         if (callback) callback(null, dest);
     }
