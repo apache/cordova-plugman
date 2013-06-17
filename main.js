587808c24830b297216f4e093d0bfa8bc3a70ef2
Anis Kadri <anis@apache.org>
2013-06-17 15:30:31 -0700

    adding fetch and fixing cli interface

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ var path = require('path')
     , nopt = require('nopt')
     , plugins = require('./src/util/plugins')
     , registry = require('plugman-registry')
+    , config = require('./config')
     , plugman = require('./plugman');
 
 var known_opts = { 'platform' : [ 'ios', 'android', 'blackberry10', 'wp7', 'wp8' ]
@@ -68,14 +69,14 @@ process.on('uncaughtException', function(error){
 if (cli_opts.v) {
     console.log(package.name + ' version ' + package.version);
 }
-else if ((cli_opts.install || cli_opts.uninstall) && (!cli_opts.platform || !cli_opts.project || !cli_opts.plugin)) {
+else if ((cli_opts.install || cli_opts.uninstall || cli_opts.argv.original.length == 0) && (!cli_opts.platform || !cli_opts.project || !cli_opts.plugin)) {
     plugman.help();
 }
 else if (cli_opts.uninstall) {
     plugman.uninstall(cli_opts.platform, cli_opts.project, cli_opts.plugin, plugins_dir, { www_dir: cli_opts.www });
 }
 else if (cli_opts.adduser) {
-  registry.use(null, function(err) {
+  registry.use(config.registry, function(err) {
     registry.adduser(null, function(err) {
       if(err) return console.log(err);
       console.log('user added');
@@ -83,7 +84,7 @@ else if (cli_opts.adduser) {
   });
 }
 else if (cli_opts.publish) {
-  registry.use(null, function(err) {
+  registry.use(config.registry, function(err) {
     registry.publish([cli_opts.plugin], function(err, d) {
       if(err) return console.log('Error publishing plugin'); 
       console.log('plugin published');
@@ -91,7 +92,7 @@ else if (cli_opts.publish) {
   });
 }
 else if (cli_opts.unpublish) {
-  registry.use(null, function(err) {
+  registry.use(config.registry, function(err) {
     registry.unpublish([cli_opts.plugin, '--force'], function(err, d) {
       if(err) return console.log('Error unpublishing plugin'); 
       console.log('plugin unpublished');
@@ -99,7 +100,7 @@ else if (cli_opts.unpublish) {
   });
 }
 else if (cli_opts.search) {
-  registry.use(null, function(err) {
+  registry.use(config.registry, function(err) {
     registry.search(cli_opts.search.split(','), function(err, d) {
       if(err) return console.log(err); 
     });
