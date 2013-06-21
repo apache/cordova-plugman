dc27fcdd0ec4fa83a56c04a1e5022e3c8412fd44
Anis Kadri <anis@apache.org>
2013-06-21 14:17:43 -0700

    adding argument check

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ else if (cli_opts.adduser) {
     });
   });
 }
-else if (cli_opts.publish) {
+else if (cli_opts.publish && cli_opts.plugin) {
   registry.use(config.registry, function(err) {
     registry.publish([cli_opts.plugin], function(err, d) {
       if(err) return console.log('Error publishing plugin', err); 
@@ -98,9 +98,9 @@ else if (cli_opts.publish) {
     });
   });
 }
-else if (cli_opts.unpublish) {
+else if (cli_opts.unpublish && cli_opts.plugin) {
   registry.use(config.registry, function(err) {
-    registry.unpublish([cli_opts.plugin, '--force'], function(err, d) {
+    registry.unpublish([cli_opts.plugin], function(err, d) {
       if(err) return console.log('Error unpublishing plugin'); 
       console.log('plugin unpublished');
     });
