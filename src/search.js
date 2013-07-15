ae545bc86310b1e355b5cc01b318145de5077cc3
Anis Kadri <anis@apache.org>
2013-07-15 14:44:46 -0700

    adding callback to search

--- src/search.js
+++ src/search.js
@@ -1,13 +1,18 @@
 var registry = require('plugman-registry')
   , config = require('../config');
 
-module.exports = function(search_opts) {
+module.exports = function(search_opts, callback) {
   registry.use(config.registry, function(err) {
     registry.search(search_opts, function(err, plugins) {
-      if(err) return console.log(err); 
-      for(var plugin in plugins) {
-        console.log(plugins[plugin].name, '-', plugins[plugin].description || 'no description provided'); 
-      }
+        if(callback) {
+            if(err) return callback(err);
+            callback(plugins);
+        } else {
+          if(err) return console.log(err);
+          for(var plugin in plugins) {
+            console.log(plugins[plugin].name, '-', plugins[plugin].description || 'no description provided'); 
+          }
+        }
     });
   });
 }
