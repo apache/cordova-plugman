f13edb19920dca7ae4622221a358d68e592533da
purplecabbage <purplecabbage@gmail.com>
2013-07-29 14:20:39 -0700

    plugins are added to www/plugins/ folder

--- src/platforms/windows8.js
+++ src/platforms/windows8.js
@@ -35,10 +35,8 @@ module.exports = {
         return manifest.find("Properties/DisplayName").text;
     },
     parseProjectFile:function(project_dir) {
-        var project_files = glob.sync('*.jsproj', {
-            cwd:project_dir
-        });
-        if (project_files.length === 0) {
+        var project_files = glob.sync('*.jsproj', { cwd:project_dir });
+        if (project_files.length == 0) {
             throw new Error(this.InvalidProjectPathError);
         }
         return new w8jsproj(path.join(project_dir, project_files[0]));
@@ -46,13 +44,15 @@ module.exports = {
     "source-file":{
         install:function(source_el, plugin_dir, project_dir, plugin_id, project_file) {
             var targetDir = source_el.attrib['target-dir'] || '';
-            var dest = path.join('Plugins', plugin_id, targetDir, path.basename(source_el.attrib['src']));
+            var dest = path.join('www', 'plugins', plugin_id, targetDir, path.basename(source_el.attrib['src']));
             common.copyFile(plugin_dir, source_el.attrib['src'], project_dir, dest);
             // add reference to this file to jsproj.
             project_file.addSourceFile(dest);
         },
         uninstall:function(source_el, project_dir, plugin_id, project_file) {
-            var dest = path.join('Plugins', plugin_id, source_el.attrib['target-dir'] ? source_el.attrib['target-dir'] : '', path.basename(source_el.attrib['src']));
+            var dest = path.join('www', 'plugins', plugin_id,
+                                 source_el.attrib['target-dir'] ? source_el.attrib['target-dir'] : '', 
+                                 path.basename(source_el.attrib['src']));
             common.removeFile(project_dir, dest);
             // remove reference to this file from csproj.
             project_file.removeSourceFile(dest);
