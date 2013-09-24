287c3d31db5a2e7b09ed9dd87b7b9de6c0f394e5
Tim Kim <timk@adobe.com>
2013-09-23 17:32:25 -0700

    [CB-4872] - updated paths to version files

--- src/util/default-engines.js
+++ src/util/default-engines.js
@@ -22,13 +22,15 @@ module.exports = function(project_dir){
         
         // TODO: these scripts have not been made!
         'apple-xcode' : 
-            { 'platform':'ios', 'scriptSrc':  path.join(project_dir,'cordova','apple-xcode-version') },
+            { 'platform':'ios', 'scriptSrc':  path.join(project_dir,'cordova','apple_xcode_version') },
         'apple-ios' : 
-            { 'platform':'ios', 'scriptSrc': path.join(project_dir,'cordova','apple-ios-version') },
+            { 'platform':'ios', 'scriptSrc': path.join(project_dir,'cordova','apple_ios_version') },
+        'apple-osx' : 
+            { 'platform':'ios', 'scriptSrc': path.join(project_dir,'cordova','apple_osx_version') },
         'blackberry-webworks' : 
             { 'platform':'blackberry10', 'scriptSrc': path.join(project_dir,'blackberry-webworks-version') },
         'android-sdk' : 
             // will have to parse string output from android list targets
-            { 'platform':'android', 'scriptSrc': path.join(project_dir,'cordova','android-sdk-version') }
+            { 'platform':'android', 'scriptSrc': path.join(project_dir,'cordova','android_sdk_version') }
     }
 };
