09bd05383075d1f8d65d4e01e528156432677fc4
ignisvulpis <axel@nennker.de>
2014-02-21 22:24:01 +0100

    CB-6088: look for config.xml in www_dir and in project_dir

--- src/platforms/firefoxos.js
+++ src/platforms/firefoxos.js
@@ -1,4 +1,5 @@
 var path = require('path')
+    , fs = require('fs')
     , common = require('./common');
 
 module.exports = {
@@ -6,7 +7,21 @@ module.exports = {
         return path.join(project_dir, 'www');
     },
     package_name:function(project_dir) {
-        var config_path = path.join(module.exports.www_dir(project_dir), 'config.xml');
+        // preferred location if cordova >= 3.4
+        var preferred_path = path.join(project_dir, 'config.xml');
+        if (!fs.existsSync(preferred_path)) {
+            // older location
+            old_config_path = path.join(module.exports.www_dir(project_dir), 'config.xml');
+            if (!fs.existsSync(old_config_path)) {
+                // output newer location and fail reading
+                config_path = preferred_path;
+                require('../../plugman').emit('verbose', 'unable to find '+config_path);
+            } else {
+                config_path = old_config_path;
+            }
+        } else {
+            config_path = preferred_path;
+        }
         var widget_doc = xml_helpers.parseElementtreeSync(config_path);
         return widget_doc._root.attrib['id'];
     },
