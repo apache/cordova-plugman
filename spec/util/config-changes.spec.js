e6a65d14dc87503a085d1dedaae0d57672366e66
Josh Soref <jsoref@blackberry.com>
2014-03-06 17:50:38 -0500

    Spelling: different

--- spec/util/config-changes.spec.js
+++ spec/util/config-changes.spec.js
@@ -180,7 +180,7 @@ describe('config-changes module', function() {
                 expect(munge['AndroidManifest.xml']['/manifest']['<!--library-->']).not.toBeDefined();
                 expect(munge['AndroidManifest.xml']['/manifest']['<!-- GCM connects to Google Services. -->']).not.toBeDefined();
             });
-            it('should increment config heirarchy leaves if dfferent config-file elements target the same file + selector + xml', function() {
+            it('should increment config heirarchy leaves if different config-file elements target the same file + selector + xml', function() {
                 var munger = new configChanges.PlatformMunger('android', temp, 'unused');
                 var munge = munger.generate_plugin_config_munge(configplugin, {});
                 expect(munge['res/xml/config.xml']['/widget']['<poop />']).toEqual(2);
