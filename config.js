39f5d143317c5b55737ce53a30371c59930efb96
Andrew Grieve <agrieve@chromium.org>
2014-06-20 22:04:16 -0400

    Revert "testing config.js"
    This reverts commit 7114350bd99b1c0c45c5d7b426dcb302df998fff.

--- config.js
+++ config.js
@@ -17,6 +17,5 @@
     under the License.
 */
 module.exports = {
-//    registry: "http://registry.cordova.io"
-    registry: "http://localhost:5984/registry/_design/app/_rewrite"
+  registry: "http://registry.cordova.io"
 }
