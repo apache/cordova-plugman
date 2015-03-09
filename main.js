105dc5d88e805313ae98cc904b6055f30ef81177
Staci Cooper <smcooper@us.ibm.com>
2015-03-09 16:07:35 -0400

    CB-8637 add windows to plugman
    Fixes error when trying to add plugin for platform windows using plugman
    directly.

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ try {
     process.exit(2);
 }
 
-var known_opts = { 'platform' : [ 'ios', 'android', 'amazon-fireos', 'blackberry10', 'wp8' , 'windows8', 'firefoxos' ]
+var known_opts = { 'platform' : [ 'ios', 'android', 'amazon-fireos', 'blackberry10', 'wp8' , 'windows8', 'windows', 'firefoxos' ]
         , 'project' : path
         , 'plugin' : [String, path, url, Array]
         , 'version' : Boolean
